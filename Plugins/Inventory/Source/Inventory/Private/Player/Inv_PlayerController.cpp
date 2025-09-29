1a86346255a6da76a722448eaffa5466177c2cc4
Nathan Guerin <nathan.guerin87@gmail.com>
2025-09-29 16:34:39 +0200

    Feat: HUD Widget

--- Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
@@ -5,6 +5,14 @@
 
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
+#include "Widgets/HUD/Inv_HUDWidget.h"
+
+void AInv_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CreateHUDWidget();
+}
 
 void AInv_PlayerController::SetupInputComponent()
 {
@@ -31,4 +39,15 @@ void AInv_PlayerController::SetupInputComponent()
 void AInv_PlayerController::PrimaryInteract()
 {
 	UE_LOG(LogTemp, Log, TEXT("Primary Interact"))
+}
+
+void AInv_PlayerController::CreateHUDWidget()
+{
+	if (!IsLocalController()) return;
+
+	HUDWidget = CreateWidget<UInv_HUDWidget>(this, HUDWidgetClass);
+	if (IsValid(HUDWidget))
+	{
+		HUDWidget->AddToViewport();
+	}
 }
--- Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
+++ Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
@@ -6,6 +6,7 @@
 #include "GameFramework/PlayerController.h"
 #include "Inv_PlayerController.generated.h"
 
+class UInv_HUDWidget;
 class UInputAction;
 class UInputMappingContext;
 /**
@@ -17,14 +18,22 @@ class INVENTORY_API AInv_PlayerController : public APlayerController
 	GENERATED_BODY()
 
 protected:
+	virtual void BeginPlay() override;
 	virtual void SetupInputComponent() override;
 
 private:
 	void PrimaryInteract();
+	void CreateHUDWidget();
 	
 	UPROPERTY(EditDefaultsOnly, Category = "Inventory|Input")
 	TArray<TObjectPtr<UInputMappingContext>> DefaultIMCs;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Inventory|Input")
 	TObjectPtr<UInputAction> PrimaryInteractAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TSubclassOf<UInv_HUDWidget> HUDWidgetClass;
+
+	UPROPERTY()
+	TObjectPtr<UInv_HUDWidget> HUDWidget;
 };
