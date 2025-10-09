87a20f2ae0350664385d17a209b0ec17b59b93df
Nathan Guerin <nathan.guerin87@gmail.com>
2025-10-09 16:03:08 +0200

    Feat: Grid Slot Events

--- Plugins/Inventory/Source/Inventory/Private/Widgets/Inventory/GridSlots/Inv_GridSlot.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/Inventory/GridSlots/Inv_GridSlot.cpp
@@ -6,6 +6,24 @@
 #include "Components/Image.h"
 #include "Items/Inv_InventoryItem.h"
 
+FReply UInv_GridSlot::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	GridSlotClicked.Broadcast(TileIndex, InMouseEvent);
+	return FReply::Handled();
+}
+
+void UInv_GridSlot::NativeOnMouseEnter(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	Super::NativeOnMouseEnter(InGeometry, InMouseEvent);
+	GridSlotHovered.Broadcast(TileIndex, InMouseEvent);
+}
+
+void UInv_GridSlot::NativeOnMouseLeave(const FPointerEvent& InMouseEvent)
+{
+	Super::NativeOnMouseLeave(InMouseEvent);
+	GridSlotUnhovered.Broadcast(TileIndex, InMouseEvent);
+}
+
 void UInv_GridSlot::SetInventoryItem(UInv_InventoryItem* Item)
 {
 	InventoryItem = Item;
