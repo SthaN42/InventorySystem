cd5633341705fe30771469b60b1ff45bf470aa40
Nathan Guerin <nathan.guerin87@gmail.com>
2025-11-19 17:08:09 +0100

    Feat: Consume Modifiers

--- Plugins/Inventory/Source/Inventory/Private/Items/Fragments/Inv_ItemFragment.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Items/Fragments/Inv_ItemFragment.cpp
@@ -72,16 +72,53 @@ void FInv_LabeledNumberFragment::Assimilate(UInv_CompositeBase* Composite) const
 	LabeledValue->SetText_Value(FText::AsNumber(Value, &Options), bCollapseValue);
 }
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+/// Consumables
+///
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+void FInv_ConsumableFragment::Assimilate(UInv_CompositeBase* Composite) const
+{
+	FInv_InventoryItemFragment::Assimilate(Composite);
+	for (const auto& Modifier : ConsumeModifiers)
+	{
+		const auto& ModRef = Modifier.Get();
+		ModRef.Assimilate(Composite);
+	}
+}
+
+void FInv_ConsumableFragment::Manifest()
+{
+	FInv_InventoryItemFragment::Manifest();
+	for (auto& Modifier : ConsumeModifiers)
+	{
+		auto& ModRef = Modifier.GetMutable();
+		ModRef.Manifest();
+	}
+}
+
+void FInv_ConsumableFragment::OnConsume(APlayerController* PC)
+{
+	for (auto& Modifier : ConsumeModifiers)
+	{
+		auto& ModRef = Modifier.GetMutable();
+		ModRef.OnConsume(PC);
+	}
+}
+
 void FInv_HealthPotionFragment::OnConsume(APlayerController* PC)
 {
 	// Get a stats component from the PC or the PC->GetPawn()
 	// or get the Ability System Component and apply a Gameplay Effect
 	// or call an interface function for Healing()
 
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("Health Potion consumed. Healing by :%f"), HealAmount));
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("Health Potion consumed. Healing by :%f"), GetValue()));
 }
 
 void FInv_ManaPotionFragment::OnConsume(APlayerController* PC)
 {
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, FString::Printf(TEXT("Mana Potion consumed. Mana replenished by :%f"), ManaAmount));
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, FString::Printf(TEXT("Mana Potion consumed. Mana replenished by :%f"), GetValue()));
 }
--- Plugins/Inventory/Source/Inventory/Public/Items/Fragments/Inv_ItemFragment.h
+++ Plugins/Inventory/Source/Inventory/Public/Items/Fragments/Inv_ItemFragment.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameplayTagContainer.h"
 #include "Inv_FragmentTags.h"
+#include "StructUtils/InstancedStruct.h"
 #include "Widgets/Composite/Inv_CompositeBase.h"
 #include "Windows/WindowsApplication.h"
 #include "Inv_ItemFragment.generated.h"
@@ -123,6 +124,8 @@ struct FInv_LabeledNumberFragment : public FInv_InventoryItemFragment
 	UPROPERTY(EditAnywhere, Category = "Inventory", meta = (DisplayAfter = "bCollapseValue", EditCondition = "!bCollapseValue", EditConditionHides))
 	bool bRandomizeValueOnManifest{true};
 
+	float GetValue() const { return Value; }
+
 private:
 	UPROPERTY(EditAnywhere, Category = "Inventory|Label")
 	bool bCollapseLabel{false};
@@ -172,8 +175,24 @@ private:
 	int32 StackCount{1};
 };
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+/// Consumables
+///
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+USTRUCT(BlueprintType, DisplayName = "Consume Modifier")
+struct FInv_ConsumeModifier : public FInv_LabeledNumberFragment
+{
+	GENERATED_BODY()
+	
+	virtual void OnConsume(APlayerController* PC) {}
+};
+
 USTRUCT(BlueprintType, DisplayName = "Consumable Fragment")
-struct FInv_ConsumableFragment : public FInv_ItemFragment
+struct FInv_ConsumableFragment : public FInv_InventoryItemFragment
 {
 	GENERATED_BODY()
 
@@ -182,27 +201,28 @@ struct FInv_ConsumableFragment : public FInv_ItemFragment
 		SetFragmentTag(FragmentTags::ConsumableFragment);
 	}
 
-	virtual void OnConsume(APlayerController* PC) {}
+	virtual void Assimilate(UInv_CompositeBase* Composite) const override;
+	virtual void Manifest() override;
+
+	virtual void OnConsume(APlayerController* PC);
+	
+private:
+	UPROPERTY(EditAnywhere, Category = "Inventory", meta = (ExcludeBaseStruct))
+	TArray<TInstancedStruct<FInv_ConsumeModifier>> ConsumeModifiers;
 };
 
 USTRUCT(BlueprintType, DisplayName = "Health Potion Fragment")
-struct FInv_HealthPotionFragment : public FInv_ConsumableFragment
+struct FInv_HealthPotionFragment : public FInv_ConsumeModifier
 {
 	GENERATED_BODY()
-
-	UPROPERTY(EditAnywhere, Category = "Inventory")
-	float HealAmount{20.f};
 
 	virtual void OnConsume(APlayerController* PC) override;
 };
 
 USTRUCT(BlueprintType, DisplayName = "Mana Potion Fragment")
-struct FInv_ManaPotionFragment : public FInv_ConsumableFragment
+struct FInv_ManaPotionFragment : public FInv_ConsumeModifier
 {
 	GENERATED_BODY()
-
-	UPROPERTY(EditAnywhere, Category = "Inventory")
-	float ManaAmount{20.f};
 
 	virtual void OnConsume(APlayerController* PC) override;
 };
