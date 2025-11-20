93c56903d9e4742ba2b8e0d9fe7872ad2770ef54
Nathan Guerin <nathan.guerin87@gmail.com>
2025-11-20 18:11:18 +0100

    Feat: Completing the Item Description

--- Plugins/Inventory/Source/Inventory/Private/Items/Fragments/Inv_FragmentTags.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Items/Fragments/Inv_FragmentTags.cpp
@@ -11,6 +11,10 @@ namespace FragmentTags
 
 	UE_DEFINE_GAMEPLAY_TAG(ItemNameFragment, "FragmentTags.ItemNameFragment");
 	UE_DEFINE_GAMEPLAY_TAG(PrimaryStatFragment, "FragmentTags.PrimaryStatFragment");
+	UE_DEFINE_GAMEPLAY_TAG(ItemTypeFragment, "FragmentTags.ItemTypeFragment");
+	UE_DEFINE_GAMEPLAY_TAG(FlavorTextFragment, "FragmentTags.FlavorTextFragment");
+	UE_DEFINE_GAMEPLAY_TAG(SellValueFragment, "FragmentTags.SellValueFragment");
+	UE_DEFINE_GAMEPLAY_TAG(RequiredLevelFragment, "FragmentTags.RequiredLevelFragment");
 
 	namespace StatMod
 	{
